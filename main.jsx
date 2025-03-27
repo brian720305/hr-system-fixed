5c86fa54522a481cb8cc6cb68d540705f4d9bf69
brian720305 <brian720417@gmail.com>
2025-03-27 15:28:42 +0800

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+function App() {
+  return <h1>HR 系統首頁（完整版）</h1>
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
